1151783d1a7420c1c83ecd932956462eff806ea1
Shridhar Damle <shridhardamle@gmail.com>
2017-03-30 15:11:48 +0530

    added workspace and a fat lib

---
+++ FooBarLib/Output/FooBarLib-Release-iphoneuniversal/FooBarLib.framework/Headers/FooBarLib.h
@@ -0,0 +1,19 @@
+//
+//  FooBarLib.h
+//  FooBarLib
+//
+//  Created by Shridhar Damale on 30/03/17.
+//  Copyright © 2017 Shridhar Damale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FooBarLib.
+FOUNDATION_EXPORT double FooBarLibVersionNumber;
+
+//! Project version string for FooBarLib.
+FOUNDATION_EXPORT const unsigned char FooBarLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FooBarLib/PublicHeader.h>
+
+
