335d1ab7b7781d5ed1d65e06a9224186c8330e0d
Shridhar Damle <shridhardamle@gmail.com>
2017-03-30 11:56:40 +0530

    Add sample lib

---
+++ FooBarLib/FooBarClass.h
@@ -0,0 +1,13 @@
+//
+//  FooBarClass.h
+//  FooBarLib
+//
+//  Created by Shridhar Damale on 30/03/17.
+//  Copyright © 2017 Shridhar Damale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FooBarClass : NSObject
+-(bool) fooBarFunc;
+@end
---
+++ FooBarLib/FooBarClass.m
@@ -0,0 +1,16 @@
+//
+//  FooBarClass.m
+//  FooBarLib
+//
+//  Created by Shridhar Damale on 30/03/17.
+//  Copyright © 2017 Shridhar Damale. All rights reserved.
+//
+
+#import "FooBarClass.h"
+
+@implementation FooBarClass
+-(bool) fooBarFunc
+{
+    return YES;
+}
+@end
